ab353f2057724c841659aaadc1bfc6abcd3e767b
wyattfry <wyattfry@gmail.com>
2018-02-03 16:40:54 -0800

    Render video player for video clicked and searched

--- src/components/videoListEntry.js
+++ src/components/videoListEntry.js
@@ -2,7 +2,7 @@ angular.module('video-player')
   .component('videoListEntry', {
     // TODO
     bindings: {
-      setVideo: '<',
+      onClick: '<',
       videos: '<',
       video: '<',
       index: '<'
