5f871531d813d0a383d866795ec7dec8c657deb7
wyattfry <wyattfry@gmail.com>
2018-02-03 16:35:58 -0800

    Search for a user-specified video

--- src/components/app.js
+++ src/components/app.js
@@ -5,13 +5,15 @@ angular.module('video-player')
     templateUrl: 'src/templates/app.html',
     controller: 'videoCtrl'
   })
-  .controller('videoCtrl', function($scope) {
+  .controller('videoCtrl', function($scope, youTube) {
     this.videos = window.exampleVideoData;
     this.currentVideo = window.exampleVideoData[0];
     this.selectVideo = (video) => {
       this.currentVideo = video;
     };
-    this.searchResults = () => {
-      console.log('search button clicked at', new Date().getTime());
+    this.setVideos = (videos) => this.videos = videos;
+    this.searchResults = (userInput) => {
+      youTube.search(userInput, this.setVideos);
+      //console.log('search button clicked at', new Date().getTime());
     };
   });
--- src/services/youTube.js
+++ src/services/youTube.js
@@ -1,4 +1,21 @@
 angular.module('video-player')
-  .service('youTube', function() {
-    // TODO
-  });
+  .service('youTube', function($http) {
+  // TODO
+  this.search = function(query, callback) {
+    $http({
+      method: 'GET',
+      url: 'https://www.googleapis.com/youtube/v3/search',
+      dataType: 'json',
+      params: {
+        q: query,
+        maxResults: 5,
+        key: window.YOUTUBE_API_KEY,
+        part: 'snippet',
+        type: 'video'
+      },
+    }).then(function (response) {
+      console.log('response', response);
+      callback(response.data.items);
+    });
+  };
+});
